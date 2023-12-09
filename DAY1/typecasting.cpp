f37a96ce78daf96e5575c5fe891f3e760196adc0
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-12-10 00:17:35 +0530

    main

---
+++ DAY1/typecasting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >> x;
+    float y = (float)x;   // typecasting
+    cout << y/2;
+}
+
+
+// Hierarchy of operators
---
+++ DAY1/userInput.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout << "Enter a Number : ";
+    cin >> x;
+    cout <<"Square of this number is : ";
+    cout << x*x;
+}
