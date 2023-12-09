1a028f14c43979de6e00183a58e74c16647bdbcd
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-12-09 23:40:21 +0530

    main

--- DAY1/basics.cpp
+++ DAY1/basics.cpp
@@ -1,5 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"Hello World !";
+int main()
+{
+    // cout << "Hello World !";
+
+
+    float r = 4.3;
+    float area = 3.1415 * r * r;
+    cout << "Area is : ";
+    cout<<area<<endl;
 }
---
+++ DAY1/si.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float p = 1000;
+    float r = 30;
+    float t = 3;
+    float si = (p*r*t)/100;
+    cout<< "Simple Interest is : ";
+    cout << si << endl;
+}
