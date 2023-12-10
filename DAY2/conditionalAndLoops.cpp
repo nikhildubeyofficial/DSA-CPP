659e46ea21641b449e5fc2255327992bb886ca34
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-12-10 22:21:10 +0530

    DAY 2

---
+++ DAY2/conditionalAndLoops.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int n;
+    // cin >> n;
+
+    // cout << "Value of n : " << n;
+
+    // if(n>0){
+    //     cout<< "Positive";
+    // }
+    // else{
+    //     cout<<"Negative";
+    // }
+
+
+    // int a,b;
+    // cout << "Enter the value of a : " << endl;
+    // cin>>a;
+    // cout << "Enter the value of b : " << endl;
+    // cin>>b;
+    // if(a>b){
+    //     cout << "A is greater" << endl;
+    // }
+    // else{
+    //     cout << "B is greater" << endl;
+    // }
+
+    
+
+
+
+
+}
