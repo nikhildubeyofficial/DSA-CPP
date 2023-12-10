d60b000e5e39fbc77195a3ef675a52ea97b1b9ae
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-12-10 23:12:15 +0530

    DAY 2

--- DAY2/conditionalAndLoops.cpp
+++ DAY2/conditionalAndLoops.cpp
@@ -27,9 +27,37 @@ int main(){
     //     cout << "B is greater" << endl;
     // }
 
-    
+    // int a;
+    // cout <<"Enter the number" <<endl;
+    // cin>>a;
+
+    // if(a>0){
+    //     cout<<"Positive";
+    // }
+    // else if(a<0){
+    //     cout<<"Negative";
+    // }
+    // else{
+    //     cout<<"Zero";
+    // }
 
 
+
+    // char ch;
+    // cout << "Enter the character : " <<endl;
+    // cin>> ch;
+    // if (ch>='a' && ch<='z')
+    // {
+    //     cout<<"Lowercase";
+    // }
+    // else if(ch>='A' && ch<='Z')
+    // {
+    //     cout<<"Uppercase";
+    // }
+    // else if(ch>='0' && ch<='9')
+    // {
+    //     cout<<"Numeric";
+    // }
 
 
 }
---
+++ DAY2/whileLoop.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >>n;
+
+    // int i = 1;
+
+    // while(i<=n){
+    //     cout<<i<<" ";
+    //     i = i + 1;
+    // }
+
+
+    int sum = 0;
+    // while(i<=n){
+    //     sum = sum + i;
+    //     i = i +1;
+    // }
+
+    // cout << "Value of sum is : " << sum;
+
+    // int i = 2;
+    // while(i<=n){
+    //     sum = sum + i;
+    //     i=i+2;
+    // }
+
+    // cout << "Value of sum of even numbers is : " << sum;
+}
