f62d3038f83537e141e537a51ea2182cff80f4bf
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-12-12 23:44:13 +0530

    main

---
+++ DAY4/Pattern13.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    int row = 1;
+
+    while (row <= n)
+    {
+
+        // space
+        int space = n - row;
+        while (space)
+        {
+            cout << " ";
+            space = space - 1;
+        }
+
+        // 2nd triangle
+        int j = 1;
+        while (j <= row)
+        {
+            cout << j;
+            j = j + 1;
+        }
+
+        // 3rd triangle
+        int start = row - 1;
+        while (start)
+        {
+            cout << start;
+            start = start - 1;
+        }
+        cout << endl;
+        row = row + 1;
+    }
+}
