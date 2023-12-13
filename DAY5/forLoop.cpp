af3a84d29c09bc370e762e6bf64071e6e84089d7
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-12-13 22:25:46 +0530

    DAY5

---
+++ DAY5/forLoop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout << "Enter the value of n " << endl;
+    cin >>n;
+
+    cout << "Printing count from 1 ot n" << endl;
+
+    for(int i= 1; i<=n; i++){
+        cout << i << endl;
+    }
+}
