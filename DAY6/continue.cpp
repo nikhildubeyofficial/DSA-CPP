bdcfa44957a0e069a63e267b73eb9434a1822456
Nikhil Dubey <officialnikhildubey@gmail.com>
2023-12-14 14:23:26 +0530

    main

---
+++ DAY6/continue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=0; i<5; i++){
+        cout << "Hi" << endl;
+        cout << "Hey" << endl;
+
+        continue;
+
+        cout << "Reply krde !" << endl;
+    }
+}
---
+++ DAY6/primeNumber.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout << "Enter the value of n: " << endl;
+    cin >> n;
+
+    bool isPrime = 1;
+
+    for(int i = 2; i<n; i++){
+        if(n%i == 0){
+            cout << "Not a Prime Number" << endl;
+            isPrime = 0;
+            break;
+        }
+    }
+
+    if(isPrime == 0){
+        cout << "Not a Prime Number" << endl;
+    }
+    else{
+        cout << "Is a Prime Number" << endl;
+    }
+}
